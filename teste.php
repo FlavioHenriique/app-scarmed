b37f639eaa591f98607f18fbf0e87f8ddcfc48c1
FlavioHenriique <flaviohenrique638@gmail.com>
2020-05-01 14:33:36 -0300

    teste

---
+++ teste.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>Scarmed</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="js/sweetalert.min.js"></script>
+    <script src="js/bootstrap.min.js"
+            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+            crossorigin="anonymous"></script>
+</head>
+<body>
+    <section class="container-fluid">
+        <section class="row">
+            <section class="col-12 col-sm-6 col-md-3">
+                <form>
+                    <div class="form-group">
+                        <h4 class="light">Teste</h4>
+                        <input class="form-control" type="email" placeholder="Email"/>
+                        <input class="form-control" type="password" placeholder="Senha"/>
+                        <input type="submit" class="btn-primary btn" value="Cadastrar">
+                    </div>
+                </form>
+            </section>
+        </section>
+    </section>
+</body>
+</html>
