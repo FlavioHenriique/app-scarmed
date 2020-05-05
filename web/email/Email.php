2bf8be4ecbbe7734eb0210d75cca9a7ade26cdbe
FlavioHenriique <flaviohenrique638@gmail.com>
2020-05-05 20:09:40 -0300

    teste

--- web/email/Email.php
+++ web/email/Email.php
@@ -16,12 +16,12 @@ function enviaEmail($destinatario, $nome){
         echo "enviando email";
         $mail->SMTPDebug = 2;
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->Host       = 'smtp.uol.com.br';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'flaviohenrique638@gmail.com';                     // SMTP username
-        $mail->Password   = 'rasenshuriken';                               // SMTP password
+        $mail->Username   = 'apnno@icloud.com';                     // SMTP username
+        $mail->Password   = 'Scar2k19';                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $mail->Port       = "587";
 
         //Recipients
         $mail->setFrom('flaviohenrique638@gmail.com', 'Scarmed');
