a5b823b2f1cdcafded39782ef570073c5d09c7d7
FlavioHenriique <flaviohenrique638@gmail.com>
2020-05-05 21:33:34 -0300

    teste

--- teste.php
+++ teste.php
@@ -9,11 +9,12 @@ $email = new PHPMailer();
 $email->SMTPDebug = 2;
 $email->isSMTP();
 $email->SMTPAuth = true;
-$email->Port = "587";
-$email->Host = "smtps.uol.com.br";
-$email->Username = "apnno@icloud.com";
-$email->Password = "Scar2k19";
-$email->SetFrom("apnno@icloud.com" , "teste");
+$email->Port = 587;
+$email->Host = "smtp.gmail.com";
+$email->Username = "flaviohenrique638@gmail.com";
+$email->SMTPSecure = PhpMailer::ENCRYPTION_STARTTLS;
+$email->Password = "rasenshuriken";
+$email->SetFrom("flaviohenrique638@gmail.com" , "teste");
 
 $email->AddAddress("jaquelira4@gmail.com" , "teste"); //Já troquei aqui pra outra pessoa e também nada !
 $email->Subject = "Testando o envio";
