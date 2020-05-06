e21b41eb62528a9bc201880ffffd5fd9c27d5d36
FlavioHenriique <flaviohenrique638@gmail.com>
2020-05-05 21:27:56 -0300

    teste

--- teste.php
+++ teste.php
@@ -11,9 +11,9 @@ $email->isSMTP();
 $email->SMTPAuth = true;
 $email->Port = "587";
 $email->Host = "smtps.uol.com.br";
-$email->Username = "scarmed@scarmed.com.br";
+$email->Username = "apnno@icloud.com";
 $email->Password = "Scar2k19";
-$email->SetFrom("flaviohenrique638@gmail.com" , "teste");
+$email->SetFrom("apnno@icloud.com" , "teste");
 
 $email->AddAddress("jaquelira4@gmail.com" , "teste"); //Já troquei aqui pra outra pessoa e também nada !
 $email->Subject = "Testando o envio";
